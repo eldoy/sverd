51e397aeee4beca46ebe699909739d9cb6a3b535
fugroup <vidar@fugroup.net>
2019-12-28 09:37:38 +0100

    Contact with api

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const loader = require('conficurse')
 const sh = require('shelljs')
-const api = require('@vultr/vultr-node')
+const vultr = require('@vultr/vultr-node')
+const config = require('./sverd.config.js')
+
+console.log(config.key)
+const api = vultr.initialize({ apiKey: config.key })
 
 module.exports = async function(args) {
   console.log('Hello')
+  console.log(config)
+
+  const info = await api.account.getInfo()
+  console.log(info)
+
 
 }
