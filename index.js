f26921d0d0cafe93f13b6769178863c47540d74e
Vidar Eldøy <vidar@eldoy.com>
2020-01-14 21:59:55 +0100

    Rename boot to server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const boot = require('./lib/boot.js')
+const server = require('./lib/server.js')
 const install = require('./lib/install.js')
 const update = require('./lib/update.js')
 const deploy = require('./lib/deploy.js')
 const help = require('./lib/help.js')
 
-module.exports = { boot, install, update, deploy, help }
+module.exports = { server, install, update, deploy, help }
